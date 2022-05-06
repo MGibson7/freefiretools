ebadb03f76663c06808686dd1a0315868d566f4e
MGibson7 <MGibsonASC@gmail.com>
2022-05-06 11:08:49 -0400

    basic financial tools site layout

---
+++ initialpage.js
@@ -0,0 +1,10 @@
+function pageLoad(){
+    const javaContent = document.getElementById("javaContent");
+    javaContent.innerHTML = ("<img src = ./capital.webp> <div id ='newCont'><h3>at free fire tools we're so concerned with efficient capital allocation that we don't even waste capital on our words. </h3></div>");
+
+}
+
+export{
+    pageLoad
+};
+ 
---
+++ podcast.js
@@ -0,0 +1,9 @@
+function podcasting(){
+    const javaContent = document.getElementById("javaContent");
+    javaContent.innerHTML = ("<img src = ./podcast.png> <div id ='newCont'><h3>our podcast is coming soon! please subscribe to be alerted when it debuts and for quick daily fire tips and challenges sent to your email</h3></div>");
+
+}
+
+export{
+    podcasting
+};
