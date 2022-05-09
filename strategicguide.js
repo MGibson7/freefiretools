be3d36485f010dec367a1a363b3dde4600cb1ad2
MGibson7 <MGibsonASC@gmail.com>
2022-05-09 15:14:46 -0400

    added slideshow and mobile optimization features

--- strategicguide.js
+++ strategicguide.js
@@ -1,8 +1,55 @@
 function journey(){
     const javaContent = document.getElementById("javaContent");
     javaContent.innerHTML = ("<img src = ./journey.png> <div id ='newCont'><h3>this quick slideshow displays by age what to pursue and what to watch out for on your journey to financial independence</h3></div>");
+    
+    let twenty = "<div id = twenty> <img src = ./twenty.png> </div> <div id = 'mainSlideCont'></div>";
+    let thirty = "<div id = thirty> <img src = ./thirtyfive.png></div> <div id = 'mainSlideCont'></div>";
+    let fourty = "<div id = fourty> <img src = ./fourtyfive.png> </div> <div id = 'mainSlideCont'></div>";
+    let fifty = "<div id = fifty> <img src = ./fifty.png>  </div> <div id = 'mainSlideCont'></div>";
+    let sixty = "<div id = sixty> <img src = ./sixty.webp>  </div> <div id = 'mainSlideCont'></div>";
 
-}
+    let twentyCont = "<div id = twentyCont> <h4>TWENTIES</h4> <h5>GOALS:</h5> <h5> WATCHOUT FOR: </h5> </div>";
+    let thirtyCont = "<div id = thirtyCont> <h4>THIRTIES</h4> <h5>GOALS:</h5> <h5> WATCHOUT FOR: </h5>";
+    let fourtyCont = "<div id = fourtyCont><h4>FOURTIES</h4> <h5>GOALS:</h5> <h5> WATCHOUT FOR: </h5>";
+    let fiftyCont = "<div id = fiftyCont> <h4>FIFTIES</h4> <h5>GOALS:</h5> <h5> WATCHOUT FOR: </h5>";
+    let sixtyCont = "<div id = sixtyCont> <h4>SIXTIES</h4> <h5>GOALS:</h5> <h5> WATCHOUT FOR: </h5>";
+
+    let ages = [twenty, thirty, fourty, fifty, sixty];
+    let agesCont = [twentyCont, thirtyCont, fourtyCont, fiftyCont, sixtyCont];
+    let mainSlide = document.getElementById("mainSlide");
+
+
+    
+
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    
+    let x = 0;
+
+    async function aging() {
+        for(const age in ages){
+            mainSlide.innerHTML = `${ages[age]}`
+            let mainSlideCont = document.getElementById("mainSlideCont")
+            mainSlideCont.innerHTML = `${agesCont[age]}`
+            await sleep(7000);
+            
+            }
+        aging();
+
+          }
+    aging();
+      
+    
+          
+            
+       
+       
+        
+    };
+
+
 
 export{
     journey
