88382e6db04c91e257499c9d643510e7f9644869
MGibson7 <MGibsonASC@gmail.com>
2022-05-10 12:08:05 -0400

    added links to apps

--- dist/resources.js
+++ dist/resources.js
@@ -1,7 +1,8 @@
 function resourceLoad(){
     const javaContent = document.getElementById("javaContent");
     javaContent.innerHTML = ("<img src = ./resources.png> <div id ='newCont'><h3>check out the links to some of our favorite resources below</h3></div>");
-    slideShow.innerHTML = "";
+    const mainSlide = document.getElementById("mainSlide");
+    mainSlide.innerHTML = "<div id = 'mainSlideCont'></div>";
 }
 
 export{
