08fc015f09dec8da171f028aa6b8e301fa376f7d
MGibson7 <MGibsonASC@gmail.com>
2022-05-11 07:30:28 -0400

    added resource links

--- podcast.js
+++ podcast.js
@@ -1,8 +1,7 @@
 function podcasting(){
     const javaContent = document.getElementById("javaContent");
     javaContent.innerHTML = ("<img src = ./podcast.png> <div id ='newCont'><h3>our podcast is coming soon! please subscribe to be alerted when it debuts and for quick daily fire tips and challenges sent to your email</h3></div>");
-    const mainSlide = document.getElementById("mainSlide");
-    mainSlide.innerHTML = "<div id = 'mainSlideCont'></div>";
+
     
 }
 
--- resources.js
+++ resources.js
@@ -1,8 +1,8 @@
 function resourceLoad(){
     const javaContent = document.getElementById("javaContent");
-    javaContent.innerHTML = ("<img src = ./resources.png> <div id ='newCont'><h3>check out the links to some of our favorite resources below</h3></div>");
-    const mainSlide = document.getElementById("mainSlide");
-    mainSlide.innerHTML = "<div id = 'mainSlideCont'></div>";
+    javaContent.innerHTML = ("<img src = ./resources.png> <div id ='newCont'> <h3>check out the links to some of our favorite resources below </h3>   <ul>  <li><a href='https://gallery.mailchimp.com/34a7cea33f33e45eedceea223/files/212585d4-0b93-42c6-aeda-6ec14e03ee20.pdf' target = '_blank' rel = 'noopener noreferrer'>how to get rich...slowly</a></li>  <li><a href='https://www.investopedia.com/financial-edge/1109/6-reasons-why-you-need-a-budget.aspx' target = '_blank' rel = 'noopener noreferrer'>budgets.. your bestfriend</a></li> <li><a href='https://mailchi.mp/abfa4335dbf1/financial-order-of-operations' target = '_blank' rel = 'noopener noreferrer'>when to do what?</a></li>  <li><a href='https://www.businessinsider.com/10-huge-bubbles-2009-11#housing-8' target = '_blank' rel = 'noopener noreferrer'>beware of bubbles</a></li> </ul></div>");
+
+   
 }
 
 export{
