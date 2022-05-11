6976cdf24aa80cf126a24c56284ebe3ccde8605a
MGibson7 <MGibsonASC@gmail.com>
2022-05-11 07:30:28 -0400

    added resource links

--- dist/newsletter.js
+++ dist/newsletter.js
@@ -1,15 +1,17 @@
   function slideShow(){
-    let twenty = "<div id = twenty> <img src = ./twenty.png> </div> <div id = 'mainSlideCont'></div>";
-    let thirty = "<div id = thirty> <img src = ./thirtyfive.png></div> <div id = 'mainSlideCont'></div>";
-    let fourty = "<div id = fourty> <img src = ./fourtyfive.png> </div> <div id = 'mainSlideCont'></div>";
-    let fifty = "<div id = fifty> <img src = ./fifty.png>  </div> <div id = 'mainSlideCont'></div>";
-    let sixty = "<div id = sixty> <img src = ./sixty.webp>  </div> <div id = 'mainSlideCont'></div>";
+    let twentyCont = "<div id = twentyCont> <h4>twenties</h4> <h5>goals: save at least some amount on a consistent basis to develop the habit </h5> <h5> watchout for: accumulating student loan debt for non lucrative career path </h5> </div>";
+    let thirtyCont = "<div id = thirtyCont> <h4>thirties</h4> <h5>goals: advance your career and save at least 15% of income toward retirement</h5> <h5> watchout for: buying too much house and speculative investments </h5>";
+    let fourtyCont = "<div id = fourtyCont><h4>fourties</h4> <h5>goals: save at least 25% of your income, be sure to have term life insurance</h5> <h5> watchout for: lifestyle creep </h5>";
+    let fiftyCont = "<div id = fiftyCont> <h4>fifties</h4> <h5>goals: become debt free</h5> <h5> watchout for: insurance investing products </h5>";
+    let sixtyCont = "<div id = sixtyCont> <h4>sixties</h4> <h5>goals: meet with financial advisor to discuss distribution strategy</h5> <h5> watchout for: overwithdrawing from accounts post retirement </h5>";
 
-    let twentyCont = "<div id = twentyCont> <h4>TWENTIES</h4> <h5>GOALS:</h5> <h5> WATCHOUT FOR: </h5> </div>";
-    let thirtyCont = "<div id = thirtyCont> <h4>THIRTIES</h4> <h5>GOALS:</h5> <h5> WATCHOUT FOR: </h5>";
-    let fourtyCont = "<div id = fourtyCont><h4>FOURTIES</h4> <h5>GOALS:</h5> <h5> WATCHOUT FOR: </h5>";
-    let fiftyCont = "<div id = fiftyCont> <h4>FIFTIES</h4> <h5>GOALS:</h5> <h5> WATCHOUT FOR: </h5>";
-    let sixtyCont = "<div id = sixtyCont> <h4>SIXTIES</h4> <h5>GOALS:</h5> <h5> WATCHOUT FOR: </h5>";
+    let twenty = `<div id = twenty> <img src = ./twenty.png> </div> <div id = 'mainSlideCont'>${twentyCont}</div>`;
+    let thirty = `<div id = thirty> <img src = ./thirtyfive.png></div> <div id = 'mainSlideCont'>${thirtyCont}</div>`;
+    let fourty = `<div id = fourty> <img src = ./fourtyfive.png> </div> <div id = 'mainSlideCont'>${fourtyCont}</div>`;
+    let fifty = `<div id = fifty> <img src = ./fifty.png>  </div> <div id = 'mainSlideCont'>${fiftyCont}</div>`;
+    let sixty = `<div id = sixty> <img src = ./sixty.webp>  </div> <div id = 'mainSlideCont'>${sixtyCont}</div>`;
+
+    
 
     let ages = [twenty, thirty, fourty, fifty, sixty];
     let agesCont = [twentyCont, thirtyCont, fourtyCont, fiftyCont, sixtyCont];
@@ -23,9 +25,7 @@
 
     async function aging() {
         for(const age in ages){
-            mainSlide.innerHTML = `${ages[age]}`
-            let mainSlideCont = document.getElementById("mainSlideCont")
-            mainSlideCont.innerHTML = `${agesCont[age]}`
+            mainSlide.innerHTML = `${ages[age]} `
             await sleep(7000);
 
             }
