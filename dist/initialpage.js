5e6666973bbc4cf0005fe730a51f60f02713b7ce
MGibson7 <MGibsonASC@gmail.com>
2022-05-12 10:22:46 -0400

    design change

--- dist/initialpage.js
+++ dist/initialpage.js
@@ -1,6 +1,6 @@
 function pageLoad(){
     const javaContent = document.getElementById("javaContent");
-    javaContent.innerHTML = ("<img src = ./capital.webp> <div id ='newCont'><h3>at free fire tools we're so concerned with efficient capital allocation that we don't even waste capital on our words. </h3></div>");
+    javaContent.innerHTML = ("<img src = ./dollars.webp> <div id ='newCont'><h3>at free fire tools we're so concerned with efficient capital allocation that we don't even waste capital on our words. </h3></div>");
     document.getElementById("slideShow").style.display = "none";
 
 
